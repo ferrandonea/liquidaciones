b5474bb3607a5ce59f756ee7382304e7c843454a
Fco <francisco@soyfocus.com>
2022-11-22 11:17:04 -0300

    utm function

--- utm.py
+++ utm.py
@@ -1,16 +1,20 @@
 from datetime import datetime
 import pandas as pd
 import requests
+from bs4 import BeautifulSoup
 
-def encuentra_utm(fecha_utm: datetime = datetime.now()):
-    fecha_year = fecha_utm.year
+def find_utm(date_utm: datetime = datetime.now()):
+    date_year = date_utm.year
+    date_month = date_utm.month
     url_sii = f"https://www.sii.cl/valores_y_fechas/utm/utm{fecha_year}.htm"
-    print (url_sii)
     
     r = requests.get(url_sii)
-    df = pd.read_html(r.text)
-    return fecha_utm
+    soup = BeautifulSoup(r.text, 'html.parser')
+    table_ = soup.select_one('table#table_export')
+    rows_ = table_.select('tr')    
+    date_row = rows_[date_month +1]
+    utm_value = date_row.select('td')[0].text.replace(".","")
+    return float(utm_value)
 
 
-print (type(encuentra_utm()))
     
