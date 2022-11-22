a1dd06a3f28664312252f53dd287c269a89acba7
Fco <francisco@soyfocus.com>
2022-11-22 11:24:42 -0300

    pylint + black

--- utm.py
+++ utm.py
@@ -1,16 +1,26 @@
+"""_utm.py_
+Obtiene la UTM desde el sitio del SII
+"""
 from datetime import datetime
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
 
-def find_utm(date_utm: datetime = datetime.now()):
+def find_utm(date_utm: datetime = datetime.now()) -> float:
+    """find_utm: Baja UTM desde SII
+
+    Args:
+        date_utm (datetime, optional): Fecha para el cálculo de la UTM. Defaults to datetime.now().
+
+    Returns:
+        float: UTM del mes date_utm
+    """
     date_year = date_utm.year
     date_month = date_utm.month
-    url_sii = f"https://www.sii.cl/valores_y_fechas/utm/utm{fecha_year}.htm"
+    url_sii = f"https://www.sii.cl/valores_y_fechas/utm/utm{date_year}.htm"
 
-    r = requests.get(url_sii)
-    soup = BeautifulSoup(r.text, "html.parser")
+    req_ = requests.get(url_sii, timeout=100000)
+    soup = BeautifulSoup(req_.text, "html.parser")
     table_ = soup.select_one("table#table_export")
     rows_ = table_.select("tr")
     date_row = rows_[date_month + 1]
